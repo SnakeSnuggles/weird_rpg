1de5901de1926400b1eb5a73842ef97d83062169
SnakeSnuggles <128649523+SnakeSnuggles@users.noreply.github.com>
2023-08-30 13:42:02 -0600

    Update main.cpp
    added visuals

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+#include <SFML/Graphics.hpp>
+#include <functional>
+#include <string>
+#include <sstream>
 #include <iostream>
 #include <random>
 #include <vector>
@@ -25,7 +29,7 @@ char send_message(const std::string& message, char& inputVar) {
 }
 
 int enemy_count = random(1, 4);
-std::vector<int> enemyhp(enemy_count);
+std::vector<int> enemyhp(5);
 
 
 int totalEnemyHP = 0;
@@ -39,9 +43,10 @@ void totalenemyHPcount()
 }
 
 
-void battle()
+void battlegen()
 {
     system("cls");
+
     enemy_count = random(1, 4);
     for (int i = 0; i <= enemy_count; i++)
     {
@@ -55,16 +60,18 @@ void battle()
         std::cout << " Hp: " << enemyhp[i] << "     ";
         totalenemyHPcount();
     }
+
+
     std::cout << '\n';
     std::cout << "You have: " << platerhp << '\n';
-    
+
     bool showstat = false;
 
     while (totalEnemyHP != 0 && platerhp >= 0)
     {
-        
-        if(showstat)
-        { 
+
+        if (showstat)
+        {
             for (int i = 0; i <= enemy_count; i++)
             {
                 if (enemyhp[i] > 0)
@@ -89,11 +96,12 @@ void battle()
                 {
                     continue;
                 }
-                
+
             }
             std::cout << '\n';
             std::cout << "You have: " << platerhp << '\n';
         }
+
 
         showstat = true;
         int choosenenemy;
@@ -101,7 +109,7 @@ void battle()
         int damage;
         int chance = random(1, 100);
 
-        std::cout << "Which enemy would you like to attack?: "; 
+        std::cout << "Which enemy would you like to attack?: ";
         std::cin >> choosenenemy;
 
         if (choosenenemy > enemy_count || choosenenemy <= -1)
@@ -121,11 +129,11 @@ void battle()
                 damage = random(1, 5);
                 if (chance > 4)
                 {
-                   enemyhp[choosenenemy] = enemyhp[choosenenemy] - damage;
-                   std::cout << "You attacked Goblin " << choosenenemy << " doing " << damage << " damage" << '\n';
-                   (enemyhp[choosenenemy] < damage) ? enemyhp[choosenenemy] = 0 : enemyhp[choosenenemy] -= damage;
-                   totalenemyHPcount();
-                   break;
+                    enemyhp[choosenenemy] = enemyhp[choosenenemy] - damage;
+                    std::cout << "You attacked Goblin " << choosenenemy << " doing " << damage << " damage" << '\n';
+                    (enemyhp[choosenenemy] < damage) ? enemyhp[choosenenemy] = 0 : enemyhp[choosenenemy] -= damage;
+                    totalenemyHPcount();
+                    break;
                 }
                 else
                 {
@@ -136,7 +144,7 @@ void battle()
                 damage = random(20, 40);
                 if (chance > 19)
                 {
-                    enemyhp[choosenenemy] =- damage;
+                    enemyhp[choosenenemy] = -damage;
                     std::cout << "You attacked Goblin " << choosenenemy << " doing " << damage << " damage" << '\n';
                     (enemyhp[choosenenemy] < damage) ? enemyhp[choosenenemy] = 0 : enemyhp[choosenenemy] -= damage;
                     totalenemyHPcount();
@@ -171,37 +179,42 @@ void battle()
         std::cout << "The goblins attack doing: ";
         for (int i = 0; i < enemy_count; i++)
         {
-            
             int gobdmg = random(0, 5);
-            platerhp = platerhp - gobdmg;
+            if (enemyhp[i] == 0)
+            {
+                continue;
+            }
+            else
+            {
+                platerhp -= gobdmg;
+                total += gobdmg;
+            }
 
-            total = total + gobdmg;
+
+
         }
         std::cout << total << '\n';
 
-        
+
         std::cout << "Press enter";
         std::cin.get();
         std::cin.get();
         system("cls");
     }
-    
-    return;
-}
-
-void adventure()
-{
-    battle();
-
-    for (int i = 0; i < enemy_count+1; i++)
+    for (int i = 0; i < enemy_count + 1; i++)
     {
         bills++;
     }
-    std::cout << "You have gained " << enemy_count+1 << " bills" << '\n';
-
+    std::cout << "You have gained " << enemy_count + 1 << " bills" << '\n';
+    return;
 }
 
-void store()
+void Adventure()
+{
+    battlegen();
+}
+
+void Store()
 {
     char ansforstore;
     send_message("Welcome to the store. Do you want to buy a burger (y) or (n)?: ", ansforstore);
@@ -220,8 +233,9 @@ void store()
     system("cls");
 }
 
-void eat()
+void Eat()
 {
+    system("cls");
     if (burgers >= 1) {
         std::cout << "You have eaten the burger and now you feel the power of the random. You see: ";
         burgers--;
@@ -235,14 +249,11 @@ void eat()
             std::cout << "tails\n";
         }
         std::cout << "You have gained 20hp" << '\n';
-        std::cin.get();
-        std::cin.get();
-        system("cls");
+
     }
     else {
         std::cout << "You do not have a burger\n";
-        std::cin.get();
-        std::cin.get();
+
     }
 }
 
@@ -262,39 +273,171 @@ void poo_poo()
 }
 
 
-int main() {
+sf::Text createText(const std::string& content, sf::Font& font, unsigned int fontSize,
+    const sf::Color& color, const sf::Vector2f& position) {
+    sf::Text text, var;
+    text.setFont(font);
+    text.setString(content);
+    text.setCharacterSize(fontSize);
+    text.setFillColor(color);
+    text.setPosition(position);
 
-    while (platerhp > 0) {
-        
+    return text;
+}
 
-        if (going_poop_in == 0)
-        {
-            poo_poo();
-            continue;
-        }
-        else
-        {
-            actions();
-        }
+class Button {
+public:
+    Button(sf::Vector2f position, sf::Vector2f size, const std::string& text, sf::Font& font,
+        const sf::Color& backgroundColor = sf::Color(128, 128, 128), const sf::Color& textColor = sf::Color::White)
+        : position(position), size(size), textString(text), font(font),
+        backgroundColor(backgroundColor), textColor(textColor) {
 
-        switch (whatdo) {
-            
-            case 'a':
-                adventure();
-                break;
+        background.setSize(size);
+        background.setPosition(position);
+        background.setFillColor(backgroundColor);
 
-            case 's':
-                store();
-                break;
+        buttonText.setString(text);
+        buttonText.setFont(font);
+        buttonText.setCharacterSize(24);
+        buttonText.setFillColor(textColor);
 
-
-            case 'b':
-                eat();
-                break;
-
-            default:
-                std::cout << "Not valid please try agian" << '\n';
-            }
+        sf::FloatRect textRect = buttonText.getLocalBounds();
+        buttonText.setOrigin(textRect.left + textRect.width / 2.0f,
+            textRect.top + textRect.height / 2.0f);
+        buttonText.setPosition(position.x + size.x / 2.0f, position.y + size.y / 2.0f);
     }
+
+    void draw(sf::RenderWindow& window) {
+        window.draw(background);
+        window.draw(buttonText);
+    }
+
+    bool isMouseOver(const sf::RenderWindow& window) {
+        sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+        sf::FloatRect bounds(position.x, position.y, size.x, size.y);
+        return bounds.contains(static_cast<float>(mousePosition.x), static_cast<float>(mousePosition.y));
+    }
+
+    void setOnClick(std::function<void()> onClick) {
+        onClickFunction = onClick;
+    }
+
+    void handleEvent(const sf::Event& event, sf::RenderWindow& window) {
+        if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left) {
+            if (isMouseOver(window) && onClickFunction) {
+                onClickFunction();
+            }
+        }
+    }
+
+    void setText(const std::string& text) {
+        textString = text;
+        buttonText.setString(text);
+        sf::FloatRect textRect = buttonText.getLocalBounds();
+        buttonText.setOrigin(textRect.left + textRect.width / 2.0f,
+            textRect.top + textRect.height / 2.0f);
+        buttonText.setPosition(position.x + size.x / 2.0f, position.y + size.y / 2.0f);
+    }
+
+private:
+    sf::Vector2f position;
+    sf::Vector2f size;
+    std::string textString;
+    sf::Font& font;
+    sf::Color backgroundColor;
+    sf::Color textColor;
+    sf::RectangleShape background;
+    sf::Text buttonText;
+    std::function<void()> onClickFunction;
+};
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Weird RPG");
+    window.setFramerateLimit(60);
+
+    sf::Font font;
+    if (!font.loadFromFile("C:\\Users\\User\\OneDrive\\Desktop\\visualsrpg\\gui\\gamegui\\x64\\Release\\arial.ttf")) {
+        // Handle font loading error
+        return 1;
+    }
+
+    Button adventure(sf::Vector2f(150, 250), sf::Vector2f(150, 150), "Adventure", font);
+    Button store(sf::Vector2f(350, 250), sf::Vector2f(150, 150), "Store", font);
+    Button eat(sf::Vector2f(550, 250), sf::Vector2f(150, 150), "Eat", font);
+
+    adventure.setOnClick([&]() {
+        Adventure();
+        });
+    store.setOnClick([&]() {
+        Store();
+        });
+    eat.setOnClick([&]() {
+        Eat();
+        });
+
+
+    sf::RectangleShape statseparator;
+    statseparator.setSize(sf::Vector2f(5000, 10));
+    statseparator.setPosition(0, 500);
+    statseparator.setFillColor(sf::Color::Black);
+
+    sf::RectangleShape hpboundingbox;
+    sf::RectangleShape bugerboundingbox;
+    sf::RectangleShape billboundingbox;
+
+
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            adventure.handleEvent(event, window);
+            store.handleEvent(event, window);
+            eat.handleEvent(event, window);
+        }
+
+        std::stringstream hpstring;
+        hpstring << "HP: " << platerhp;
+
+        sf::Text hpText = createText(hpstring.str(), font, 24, sf::Color::Black, sf::Vector2f(50, 540));
+        hpboundingbox.setPosition(5, 515);
+        hpboundingbox.setSize(sf::Vector2f(200, 80));
+        hpboundingbox.setOutlineColor(sf::Color::Black);
+        hpboundingbox.setOutlineThickness(2);
+
+        std::stringstream burgerstring;
+        burgerstring << "Burgers: " << burgers;
+
+        sf::Text burgerText = createText(burgerstring.str(), font, 24, sf::Color::Black, sf::Vector2f(250, 540));
+        bugerboundingbox.setPosition(210, 515);
+        bugerboundingbox.setSize(sf::Vector2f(200, 80));
+        bugerboundingbox.setOutlineColor(sf::Color::Black);
+        bugerboundingbox.setOutlineThickness(2);
+
+        std::stringstream billstring;
+        billstring << "Bills: " << bills;
+
+        sf::Text billText = createText(billstring.str(), font, 24, sf::Color::Black, sf::Vector2f(450, 540));
+        billboundingbox.setPosition(415, 515);
+        billboundingbox.setSize(sf::Vector2f(200, 80));
+        billboundingbox.setOutlineColor(sf::Color::Black);
+        billboundingbox.setOutlineThickness(2);
+
+        window.clear(sf::Color::White);
+        adventure.draw(window);
+        store.draw(window);
+        eat.draw(window);
+        window.draw(statseparator);
+        window.draw(hpboundingbox);
+        window.draw(bugerboundingbox);
+        window.draw(billboundingbox);
+        window.draw(hpText);
+        window.draw(burgerText);
+        window.draw(billText);
+        window.display();
+    }
+
     return 0;
 }
